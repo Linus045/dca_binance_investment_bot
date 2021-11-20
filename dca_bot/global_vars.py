4b1ff4d830eea29475b6ba3edcc243ffaaecfacf
Linus045 <lwillms88@gmail.com>
2021-11-20 22:01:23 +0100

    Added LOG_*_AND_NOTIFY methods to be more explicit
    + LOG_*  are reverted back to just logging
    + LOG_*_AND_NOTIFY now logs and also sends out a push notification
    + Removed notify_users field from global_vars
    + Removed unnecessary imports from global_vars which caused circular import references

--- dca_bot/global_vars.py
+++ dca_bot/global_vars.py
@@ -1,12 +1,8 @@
 # TODO: Change how Threads are stopped
 # Used to stop all running threads
-from firebase.firebase_messager import FirebaseMessager
-from firebase.firebase_storage import FirebaseStorage
-
 stop_threads = False
 
 
 # TODO: Move this
-firebaseStorage : FirebaseStorage = None
-firebaseMessager : FirebaseMessager = None
-notify_users = True
+firebaseStorage = None
+firebaseMessager = None
